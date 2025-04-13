91e216e4b8b3fb413a482c03d43ffc25a959eed1
иван шулубин <shulubin.ia@gmail.com>
2025-04-13 21:10:26 +0300

    bugges fixxed

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-set -ex  
-
-ls -la .venv/bin/gunicorn || echo "Gunicorn not found!"
-echo "Проверка Makefile:"
-cat -e -t -v Makefile
+set -ex
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+
+
+python -m venv .venv || echo "Virtual environment already exists"
+
+
+.venv/bin/pip install -r requirements.txt
+
+
+echo "Checking Gunicorn:"
+ls -la .venv/bin/gunicorn
+chmod +x .venv/bin/gunicorn
