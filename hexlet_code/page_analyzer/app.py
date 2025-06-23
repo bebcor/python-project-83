ac5b56e24fd510a8fb7fa3c6df983367719323ef
иван шулубин <shulubin.ia@gmail.com>
2025-06-23 15:44:26 +0300

    Add main page template

--- hexlet_code/page_analyzer/app.py
+++ hexlet_code/page_analyzer/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 
@@ -10,5 +10,5 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
-def root_page():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
